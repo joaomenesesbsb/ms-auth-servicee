2449d6c97483d2ddf18311bb2bc1999942a14e0e
joao_meneses <joaovic2012@hotmail.com>
2026-04-02 18:02:47 -0300

    Criada DTO LoginResponse

---
+++ src/main/java/com/meneses/auth/dto/LoginResponse.java
@@ -0,0 +1,13 @@
+package com.meneses.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginResponse {
+    private String token;
+    private String refreshtoken;
+}
---
+++ src/main/java/com/meneses/auth/services/AuthService.java
@@ -0,0 +1,7 @@
+package com.meneses.auth.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+}
