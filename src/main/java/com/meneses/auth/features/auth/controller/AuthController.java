8756c25cebd3fcc0ce6e41010734f11fad7f14c2
joao_meneses <joaovic2012@hotmail.com>
2026-04-14 22:22:34 -0300

    docs(swagger): auth

--- src/main/java/com/meneses/auth/features/auth/controller/AuthController.java
+++ src/main/java/com/meneses/auth/features/auth/controller/AuthController.java
@@ -17,28 +17,45 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 public interface AuthController {
 
-    @Operation(summary = "Login de usuario",
-            description = "Realiza login, valida senha, gera token JWT e gera refresh token"
+    @Operation(summary = "Login user",
+            description = "Log in, validate password, generate JWT token, and generate refresh token."
     )
     @ApiResponses({
-            @ApiResponse( responseCode = "200", description = "Login realizado com sucesso",  content = @Content( mediaType = "application/json", schema = @Schema(implementation = LoginResponseDTO.class))),
-            @ApiResponse( responseCode = "401",  description = "Credenciais inválidas",  content = @Content( mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
-            @ApiResponse( responseCode = "400",  description = "Dados inválidos",  content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
-            @ApiResponse( responseCode = "500",  description = "Erro interno",  content = @Content( mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))
+            @ApiResponse( responseCode = "200", description = "login successful",
+                    content = @Content( mediaType = "application/json", schema = @Schema(implementation = LoginResponseDTO.class))),
+            @ApiResponse( responseCode = "401",  description = "Invalid credentials",
+                    content = @Content( mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse( responseCode = "400",  description = "Invalid data",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse( responseCode = "500",  description = "Internal server error",
+                    content = @Content( mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))
     })
     @PostMapping("/login")
     ResponseEntity<LoginResponseDTO> login(@RequestBody LoginRequestDTO request);
 
-    @Operation(summary = "Registrar novo usuario", description = "Cria um novo usuário com role padrão USER")
+    @Operation(summary = "Register new user", description = "Creates a new user with the default USER role.")
     @ApiResponses({
-            @ApiResponse( responseCode = "201", description = "Usuário cadastrado com sucesso", content = @Content( mediaType = "application/json",  schema = @Schema(implementation = UserResponseDTO.class))),
-            @ApiResponse( responseCode = "400", description = "Dados inválidos", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
-            @ApiResponse( responseCode = "409", description = "Email já cadastrado", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
-            @ApiResponse( responseCode = "500", description = "Erro interno do servidor", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))
+            @ApiResponse( responseCode = "201", description = "User successfully registered",
+                    content = @Content( mediaType = "application/json",  schema = @Schema(implementation = UserResponseDTO.class))),
+            @ApiResponse( responseCode = "400", description = "Invalid data",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse( responseCode = "409", description = "Email already register",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse( responseCode = "500", description = "Internal server error",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))
     })
     @PostMapping("/register")
     ResponseEntity<UserResponseDTO> register(@RequestBody RegisterRequestDTO request);
 
+    @Operation(summary = "User logout",
+            description = "Invalidates the current JWT token, adding in to the blacklist in Redis.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "Logout success"),
+            @ApiResponse(responseCode = "401", description = "Invalid or missing token",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "500", description = "Internal server error",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))
+    })
     @PostMapping("/logout")
     public ResponseEntity<Void> logout(HttpServletRequest request);
 }
